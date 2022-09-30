d68f453ec7df5d01bc28d7451c21e9a57fc1a749
Lucemans <luc@lucemans.nl>
2022-09-30 11:30:01 +0200

    Update Mobile Sizing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="mx-auto w-fit">
-	<div class="max-w-insta mx-4 lg:mt-28 mb-28 flex flex-col gap-7">
+	<div class="max-w-insta sm:mx-4 lg:mt-28 mb-28 flex flex-col gap-7">
 		<img src="/banner.jpeg" alt="" class="w-full h-auto rounded-lg" />
 
 		<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-7">
